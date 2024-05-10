1f0b6cdb0cc4da6ae429ef79585e890a05840caa
lauren-lizzy-levine <lauren.lizzy.levine@gmail.com>
2024-05-09 22:59:33 -0400

    coarse relation normalization and visualization update

--- relation_variation.py
+++ relation_variation.py
@@ -4,12 +4,20 @@ from scipy.spatial.distance import squareform
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set_style('dark')
+
+
+def get_colors():
+	colors = {"organization": "red", "restatement": "darkorange", "explanation": "gold", "context": "yellow",
+			  "topic": "yellowgreen", "joint": "green", "causal": "turquoise", "contingency": "teal", "mode": "skyblue",
+			  "elaboration": "dodgerblue", "purpose": "blue", "attribution": "darkviolet", "adversative": "fuchsia",
+			  "evaluation": "hotpink"}
+
+	return colors
 
 
 def frequency_counts_rel_genre(infile, coarse=False):
 	lines = []
+	coarse_relations_set = set()
 	with open(infile, "r") as file:
 		reader = csv.reader(file, delimiter='\t')
 		next(reader, None)  # skip the headers
@@ -18,10 +26,8 @@ def frequency_counts_rel_genre(infile, coarse=False):
 	freq_counts = {}
 	for line in lines:
 		genre = line[1]
-		if coarse:
-			relation = line[2] # coarse relation
-		else:
-			relation = line[3]
+		coarse_relations_set.add(line[2])
+		relation = line[3] # fine grained relation
 		signal_type = line[4]
 		if signal_type == 'orp':
 			signal_type = 'dm'
@@ -44,11 +50,42 @@ def frequency_counts_rel_genre(infile, coarse=False):
 				if sig_type not in freq_counts[relation][genre]:
 					freq_counts[relation][genre][sig_type] = 0
 
-	# eliminate topic-solutionhood because it only occura 8 times
+	# eliminate topic-solutionhood because it only occurs 8 times
 	if "topic-solutionhood" in freq_counts:
 		freq_counts.pop("topic-solutionhood")
 
+	if coarse:
+		# normalize each fine grained relation
+		freq_counts_normalized = {}
+		for relation in freq_counts:
+			freq_counts_normalized[relation] = {}
+			for genre in freq_counts[relation]:
+				freq_counts_normalized[relation][genre] = {}
+				total = sum(freq_counts[relation][genre].values())
+				for sig_type in freq_counts[relation][genre]:
+					freq_counts_normalized[relation][genre][sig_type] = freq_counts[relation][genre][sig_type] / total
+		# collapse into coarse relations by averaging proportions of fine grained
+		collaposed_avg = {}
+		for relation in list(coarse_relations_set):
+			collaposed_avg[relation] = {}
+			n = 0
+			for rel in freq_counts_normalized:
+				if relation in rel:
+					n += 1
+					for genre in freq_counts_normalized[rel]:
+						if genre in collaposed_avg[relation]:
+							for sig_type in freq_counts_normalized[rel][genre]:
+								collaposed_avg[relation][genre][sig_type] += freq_counts_normalized[rel][genre][sig_type]
+						else:
+							collaposed_avg[relation][genre] = freq_counts_normalized[rel][genre]
+			# divide by n
+			for genre in collaposed_avg[relation]:
+				for sig_type in collaposed_avg[relation][genre]:
+					collaposed_avg[relation][genre][sig_type] = collaposed_avg[relation][genre][sig_type] / n
+		freq_counts = collaposed_avg
+
 	return freq_counts
+
 
 def pairwise_jsd(freq_counts):
 	pair_distances = {}
@@ -70,6 +107,7 @@ def pairwise_jsd(freq_counts):
 
 	return pair_distances
 
+
 def rank_variation(pair_distances):
 	# Which relations show the most inter-genre variation?
 	# For each relation, calculate pair-wise avg of JSD, then sort in decending order
@@ -83,28 +121,23 @@ def rank_variation(pair_distances):
 	relation_jsd_sorted_descending = sorted(relation_avg_jsd, key=lambda x: x[1], reverse=True)
 	for rel_jsd in relation_jsd_sorted_descending:
 		print(rel_jsd)
-		#('organization', 0.3575705520997728)
-		#('explanation', 0.34831146431631826)
-		#('restatement', 0.2965876654702925)
-		#('context', 0.2741118748162802)
-		#('joint', 0.2352734773629788)
-		#('mode', 0.19919851956411835)
-		#('causal', 0.19592911381159392)
-		#('contingency', 0.18613163957265172)
-		#('elaboration', 0.17102395611252416)
-		#('purpose', 0.11921745935527214)
-		#('attribution', 0.11417348527138776)
-		#('adversative', 0.08437153764023812)
-		#('evaluation', 0.02908569613791581)
 
 	return relation_jsd_sorted_descending
+
 
 def visualize_ranking(ranking, title, outfile):
 	categories = [x[0] for x in ranking]
 	values = [x[1] for x in ranking]
+	# make colors input
+	color_assignment = get_colors()
+	colors = []
+	for relation in categories:
+		if relation not in color_assignment:
+			relation = relation.split("-")[0]
+		colors.append(color_assignment[relation])
 	# bar plot
 	plt.figure(figsize=(8, 6))  # Optional: adjust the size of the plot
-	plt.bar(categories, values, color='skyblue')
+	plt.bar(categories, values, color=colors)
 
 	# title and labels
 	plt.title(title)
@@ -114,15 +147,13 @@ def visualize_ranking(ranking, title, outfile):
 	plt.tight_layout()
 
 	#plt.savefig('visualizations/' + outfile, bbox_inches='tight')  # Save the plot as a PNG file with tight bounding box
-	#plt.show()
+	plt.show()
 
 	return
 
-def make_distance_matrix(distances):
-	#genres = ["academic", "bio", "conversation", "court", "essay", "fiction", "interview", 
-	#"letter", "news", "podcast", "reddit", "speech", "textbook", "vlog", "voyage", "whow"]
-	attested_genres = sorted(list(set([x[0] for x in distances.keys()] + [x[1] for x in distances.keys()])))
 
+def make_distance_matrix(distances):
+	attested_genres = sorted(list(set([x[0] for x in distances.keys()] + [x[1] for x in distances.keys()])))
 	distance_matrix = []
 	for genre1 in attested_genres:
 		row = []
@@ -141,7 +172,7 @@ def make_distance_matrix(distances):
 
 
 def make_dendrogram(relation, pair_distances, outfile):
-	# Will illustrate which genres signal the gives relation most similarly/differently
+	# Illustrates which genres signal the given relation most similarly/differently
 	# Make distance matrix
 	distance_matrix, genres = make_distance_matrix(pair_distances[relation])
 	dists = squareform(distance_matrix)
@@ -153,18 +184,20 @@ def make_dendrogram(relation, pair_distances, outfile):
 	plt.tight_layout()
 	#plt.savefig('visualizations/' + outfile)  # Save the plot as a PNG file with tight bounding box
 	plt.show()
+
 	return
+
 
 def coarse_level_variation():
 	datafile = "GUM_signals.tsv"
 	freq_counts = frequency_counts_rel_genre(datafile, coarse=True)
-	print(freq_counts["context"])
 	pair_distances = pairwise_jsd(freq_counts)
 	ranking = rank_variation(pair_distances)
 	visualize_ranking(ranking, "Inter-Genre Variation of Coarse Relations", "coarse_rel_inter_genre_var.png")
-	dendro_relations = ["organization", "explanation", "causal"]
+	dendro_relations = ["organization", "restatement", "explanation"]
 	for rel in dendro_relations:
 		make_dendrogram(rel, pair_distances, "dendrogram_" + rel + ".png")
+
 	return
 
 
@@ -174,45 +207,10 @@ def fine_grained_level_variation():
 	pair_distances = pairwise_jsd(freq_counts)
 	ranking = rank_variation(pair_distances)
 	visualize_ranking(ranking, "Inter-Genre Variation of Fine-Grained Relations", "fine_grained_rel_inter_genre_var.png")
-	organization_distances = {"organization-heading": pair_distances["organization-heading"], "organization-phatic": pair_distances["organization-phatic"], 
-							"organization-preparation": pair_distances["organization-preparation"]}
-	organization_ranking = rank_variation(organization_distances)
-	visualize_ranking(organization_ranking, "Inter-Genre Variation of Organization Relations", "organization_rel_inter_genre_var.png")
-	explanation_distances = {"explanation-evidence": pair_distances["explanation-evidence"], "explanation-justify": pair_distances["explanation-justify"], 
-							"explanation-motivation": pair_distances["explanation-motivation"]}
-	explanation_ranking = rank_variation(explanation_distances)
-	visualize_ranking(explanation_ranking, "Inter-Genre Variation of Explanation Relations", "explanation_rel_inter_genre_var.png")
-	causal_distances = {"causal-cause": pair_distances["causal-cause"], "causal-result": pair_distances["causal-result"]}
-	causal_ranking = rank_variation(causal_distances)
-	visualize_ranking(causal_ranking, "Inter-Genre Variation of Causal Relations", "causal_rel_inter_genre_var.png")
-	dendro_relations = ["organization-heading", "organization-phatic", "organization-preparation", "explanation-evidence",
-						 "explanation-justify", "explanation-motivation", "causal-cause", "causal-result"]
-	for rel in dendro_relations:
-		make_dendrogram(rel, pair_distances, "dendrogram_" + rel + ".png")
 
 	return
 
-
-def test():
-	categories = ['dm', 'grf', 'lex', 'mrf', 'num', 'ref', 'sem', 'syn']
-	values = [24, 2, 11, 1, 0, 17, 91, 8]
-	# bar plot
-	plt.figure(figsize=(8, 6))  # Optional: adjust the size of the plot
-	#plt.bar(categories, values, color='skyblue')
-	plt.pie(values, labels=categories)
-
-	# title and labels
-	plt.title('Context Academic Proportions')
-	#plt.xlabel('Signal Types')
-	#plt.ylabel('Counts')
-	#plt.xticks(rotation=90)
-	plt.tight_layout()
-
-	#plt.savefig('visualizations/' + outfile, bbox_inches='tight')  # Save the plot as a PNG file with tight bounding box
-	plt.show()
-	return
 
 if __name__ == "__main__":
-	test()
-	#coarse_level_variation()
-	#fine_grained_level_variation()
+	coarse_level_variation()
+	fine_grained_level_variation()
--- visualizations.py
+++ visualizations.py
@@ -2,23 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def signal_proportion_by_genre(datafile, relation_list, relation_group_name, outfile, coarse=False):
+def signal_proportion_by_genre(datafile, relation_group_name, outfile, relation_list=None, coarse=False, normalize=False):
 	df = pd.read_csv(datafile, sep="\t")
-	# Filter to have only genres in genre_list
-	if coarse:
-		df = df[df['COARSE_RELATION'].isin(relation_list)]
-	else:
-		df = df[df['RELATION'].isin(relation_list)]
 	# Change oph label to dm
 	df['SIGNAL_TYPE'].replace('orp', 'dm', inplace=True)
+	# Filter to have only relation in relation_list
+	if relation_list is not None:
+		if coarse:
+			df = df[df['COARSE_RELATION'].isin(relation_list)]
+		else:
+			df = df[df['RELATION'].isin(relation_list)]
 	# Get value counts
 	value_counts = df['GENRE'].value_counts()
 	group_names = value_counts.index.tolist()
-	# Add constant value for each row to count the occurrences
-	df['Value'] = 1
-	# Calculate proportions
-	df['Total'] = df.groupby('GENRE')['Value'].transform('sum')
-	df['Proportion'] = df['Value'] / df['Total']
+	if normalize:
+		fine_relations = df['RELATION'].unique()
+		column_names = list(df.columns)
+		new_df = pd.DataFrame(columns=column_names + ["Value", "Total", "Proportion"])
+		for relation in fine_relations:
+			# make sub data frame
+			temp_df = df[df["RELATION"] == relation].copy()
+			# make proportions
+			# Add constant value for each row to count the occurrences
+			temp_df['Value'] = 1
+			# Calculate proportions
+			temp_df['Total'] = temp_df.groupby('GENRE')['Value'].transform('sum')
+			temp_df['Proportion'] = (temp_df['Value'] / temp_df['Total']) #/ temp_df['Relation_Count']
+			# concat onto data frame
+			new_df = pd.concat([new_df, temp_df], ignore_index=True)
+		new_df['Relation_Count'] = new_df.groupby('GENRE')['RELATION'].transform('nunique')
+		new_df['Proportion'] = new_df['Proportion'] / new_df['Relation_Count']
+		df = new_df
+	else:
+		# Add constant value for each row to count the occurrences
+		df['Value'] = 1
+		# Calculate proportions
+		df['Total'] = df.groupby('GENRE')['Value'].transform('sum')
+		df['Proportion'] = df['Value'] / df['Total']
 	# Pivot the DataFrame for plotting
 	pivot_df = df.pivot_table(index='GENRE', columns='SIGNAL_TYPE', values='Proportion', aggfunc='sum')
 	# Update pivot rows (genres) with occurrence counts
@@ -26,7 +46,6 @@ def signal_proportion_by_genre(datafile, relation_list, relation_group_name, out
 	for genre in group_names:
 		new_row_names[genre] = genre + ' (' + str(value_counts[genre]) + ')'
 	pivot_df.rename(index=new_row_names, inplace=True)
-	#print(pivot_df)
 	# Plot
 	pivot_df.plot(kind='bar', stacked=True)
 	plt.xlabel('Genre')
@@ -40,98 +59,31 @@ def signal_proportion_by_genre(datafile, relation_list, relation_group_name, out
 	return
 
 
-def fine_grained_signal_proportion_genre_graphs():
-	datafile = "GUM_signals.tsv"
-	signal_proportion_by_genre(datafile, ["organization-heading"], "Organization-heading", "organization-heading_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["organization-phatic"], "Organization-phatic", "organization-phatic_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["organization-preparation"], "Organization-preparation", "organization-preparation_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["explanation-evidence"], "Explanation-evidence", "explanation-evidence_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["explanation-justify"], "Explanation-justify", "explanation-justify_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["explanation-motivation"], "Explanation-motivation", "explanation-motivation_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["causal-cause"], "Causal-cause", "causal-cause_genre_signal.png")
-	signal_proportion_by_genre(datafile, ["causal-result"], "Causal-result", "causal-result_genre_signal.png")
-	return
-
-
 def create_signal_proportion_genre_graphs():
 	datafile = "GUM_signals.tsv"
-	signal_proportion_by_genre(datafile, ["adversative"], "Adversative", "adversative_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["attribution"], "Attribution", "attribution_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["causal"], "Causal", "causal_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["context"], "Context", "context_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["contingency"], "Contingency", "contingency_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["elaboration"], "Elaboration", "elaboration_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["evaluation"], "Evaluation", "evaluation_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["explanation"], "Explanation", "explanation_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["joint"], "Joint", "jont_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["mode"], "Mode", "mode_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["organization"], "Organization", "organization_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["purpose"], "Purpose", "purpose_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["restatement"], "Restatement", "restatement_genre_signal.png", coarse=True)
-	signal_proportion_by_genre(datafile, ["topic"], "Topic", "topic_genre_signal.png", coarse=True)
-	return
-
-
-def signal_proportion_by_relation(datafile, genre_list, genre_group_name, outfile):
-	df = pd.read_csv(datafile, sep="\t")
-	# Filter to have only genres in genre_list
-	df = df[df['GENRE'].isin(genre_list)]
-	# Change oph label to dm
-	df['SIGNAL_TYPE'].replace('orp', 'dm', inplace=True)
-	# Get value counts
-	value_counts = df['COARSE_RELATION'].value_counts()
-	group_names = value_counts.index.tolist()
-	# Add constant value for each row to count the occurrences
-	df['Value'] = 1
-	# Calculate proportions
-	df['Total'] = df.groupby('COARSE_RELATION')['Value'].transform('sum')
-	df['Proportion'] = df['Value'] / df['Total']
-	# Pivot the DataFrame for plotting
-	pivot_df = df.pivot_table(index='COARSE_RELATION', columns='SIGNAL_TYPE', values='Proportion', aggfunc='sum')
-	# Update pivot rows (coarse_relation) with occurrence counts
-	new_row_names = {}
-	for relation in group_names:
-		new_row_names[relation] = relation + ' (' + str(value_counts[relation]) + ')'
-	pivot_df.rename(index=new_row_names, inplace=True)
-	#print(pivot_df)
-	# Plot
-	pivot_df.plot(kind='bar', stacked=True)
-	plt.xlabel('Coarse Relation')
-	plt.ylabel('Proportion')
-	plt.title('Proportion of Signal Type per Coarse Relation for GUM ' + genre_group_name)
-	plt.legend(title='Signal Type', bbox_to_anchor=(1, 1))
-	plt.xticks(rotation=60)
-	plt.savefig('visualizations/' + outfile, bbox_inches='tight')  # Save the plot as a PNG file with tight bounding box
-	#plt.show()
+	signal_proportion_by_genre(datafile, "All", "all_relations_genre_signal.png",
+							   coarse=True, normalize=False)
+	signal_proportion_by_genre(datafile, "All", "all_relations_genre_signal_normal.png",
+							   coarse=True, normalize=True)
+	signal_proportion_by_genre(datafile, "Explanation-Evidence", "explanation-evidence_relations_genre_signal.png",
+							   relation_list=["explanation-evidence"], coarse=False, normalize=False)
+	signal_proportion_by_genre(datafile, "Adversative-Antithesis", "adversative-antithesis_relations_genre_signal.png",
+							   relation_list=["adversative-antithesis"], coarse=False, normalize=False)
+	signal_proportion_by_genre(datafile, "Evaluation-Comment", "evaluation-comment_relations_genre_signal.png",
+							   relation_list=["evaluation-comment"], coarse=False, normalize=False)
+	signal_proportion_by_genre(datafile, "Organization", "organization_relations_genre_signal_normal.png",
+							   relation_list=["organization"], coarse=True, normalize=True)
+	signal_proportion_by_genre(datafile, "Causal", "causal_relations_genre_signal_normal.png",
+							   relation_list=["causal"], coarse=True, normalize=True)
+	signal_proportion_by_genre(datafile, "Evaluation", "evaluation_relations_genre_signal_normal.png",
+							   relation_list=["evaluation"], coarse=True, normalize=True)
+	signal_proportion_by_genre(datafile, "Restatement", "restatement_relations_genre_signal_normal.png",
+							   relation_list=["restatement"], coarse=True, normalize=True)
+	signal_proportion_by_genre(datafile, "Explanation", "explanation_relations_genre_signal_normal.png",
+							   relation_list=["explanation"], coarse=True, normalize=True)
 
 	return
 
-def create_signal_proportion_graphs():
-	datafile = "GUM_signals.tsv"
-	signal_proportion_by_relation(datafile, ["academic"], "Academic Writing", "academic_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["bio"], "Biographies", "bio_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["court"], "Courtroom Transcripts", "court_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["essay"], "Essays", "essay_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["fiction"], "Fiction", "fiction_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["whow"], "How-to Guides", "whow_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["interview"], "Interviews", "interview_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["letter"], "Letters", "letter_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["news"], "News", "news_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["reddit"], "Reddit", "reddit_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["podcast"], "Podcasts", "podcast_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["speech"], "Political Speeches", "speech_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["conversation"], "Conversations", "conversation_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["textbook"], "Textbooks", "textbook_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["voyage"], "Travel Guides", "voyage_relation_signal.png")
-	signal_proportion_by_relation(datafile, ["vlog"], "Vlogs", "vlog_relation_signal.png")
-	written_genres = ["academic", "bio", "essay", "fiction", "whow", "letter", "news", "reddit", "textbook", "voyage"]
-	spoken_genres = ["court", "interview", "podcast", "speech", "conversation", "vlog"]
-	signal_proportion_by_relation(datafile, written_genres, "Written", "written_relation_signal.png")
-	signal_proportion_by_relation(datafile, spoken_genres, "Spoken", "spoken_relation_signal.png")
-	signal_proportion_by_relation(datafile, written_genres + spoken_genres, "All Data", "everything_relation_signal.png")
-	return
 
 if __name__ == "__main__":
-	#create_signal_proportion_graphs()
-	#create_signal_proportion_genre_graphs()
-	fine_grained_signal_proportion_genre_graphs()
+	create_signal_proportion_genre_graphs()
